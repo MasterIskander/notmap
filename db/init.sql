1010ce846222f5374d8c2680e9cd79cde30cc22b
Neuroman <ftpnineman@gmail.com>
2024-06-27 15:07:00 +0300

    add wait bd for back and bot

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,21 @@
-BEGIN
-   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'notmap') THEN
-      CREATE DATABASE notmap;
-   END IF;
-END
-
+-- Создание базы данных и пользователя
+DO
+$$
 BEGIN
    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'notmap_user') THEN
-      CREATE USER notmap_user WITH PASSWORD 'your_password';
-      GRANT ALL PRIVILEGES ON DATABASE notmap TO notmap_user;
+      CREATE ROLE notmap_user WITH LOGIN PASSWORD 'your_password';
+   END IF;
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'notmap') THEN
+      CREATE DATABASE notmap WITH OWNER = notmap_user;
    END IF;
 END
+$$;
 
-use notmap;
+\c notmap;
 
 -- Скрипт создания таблиц
 -- Создание таблицы users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     telegram_tag VARCHAR(255),
     name VARCHAR(255),
@@ -25,14 +25,14 @@ CREATE TABLE users (
 );
 
 -- Создание таблицы maps
-CREATE TABLE maps (
+CREATE TABLE IF NOT EXISTS maps (
     id SERIAL PRIMARY KEY,
     number_land INTEGER,
     field_wrap VARCHAR(255)
 );
 
 -- Создание таблицы lands
-CREATE TABLE lands (
+CREATE TABLE IF NOT EXISTS lands (
     id SERIAL PRIMARY KEY,
     coordinate VARCHAR(255),
     type VARCHAR(255),
